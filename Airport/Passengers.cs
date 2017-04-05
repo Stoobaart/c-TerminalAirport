88e47dad6531a566e9aef973256d1135947686a8
Christopher J Tsoi <chrisjtsoi.work@gmail.com>
2017-04-05 12:57:51 +0100

    Added passengers class

--- Airport/Passengers.cs
+++ Airport/Passengers.cs
@@ -4,9 +4,20 @@ namespace AirportApp
 {
 	public class Passengers
 	{
-		public Passengers()
+		private string firstName;
+		private string lastName;
+		private string dateOfBirth;
+
+		public Passengers(string inputFirstName, string inputLastName, string inputDateOfBirth)
 		{
-			
+			firstName = inputFirstName;
+			lastName = inputLastName;
+			dateOfBirth = inputDateOfBirth;
+		}
+
+		public override string ToString()
+		{
+			return String.Format("{0} {1} {2}", firstName, lastName, dateOfBirth);
 		}
 	}
 }
